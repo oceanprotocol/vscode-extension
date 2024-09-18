2e97e010b74f2f060f0d51e6c56f5268b09a89bb
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-09-18 13:42:05 +0300

    Adding ocean.js

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,73 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import { ConfigHelper, Aquarius, Config, Asset } from '@oceanprotocol/lib'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Ocean Protocol extension is now active!')
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ocean-protocol" is now active!');
+  // Command to search for assets
+  let searchAssets = vscode.commands.registerCommand(
+    'ocean-protocol.searchAssets',
+    async () => {
+      const searchTerm = await vscode.window.showInputBox({
+        prompt: 'Enter search term for Ocean assets'
+      })
+      if (searchTerm) {
+        try {
+          const config = getOceanConfig()
+          const aquarius = new Aquarius(config.metadataCacheUri)
+          const query = { query: { query_string: { query: searchTerm } } }
+          const result = await aquarius.querySearch(query)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ocean-protocol.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ocean-protocol!');
-	});
+          if (result && result.hits && result.hits.hits) {
+            const assets = result.hits.hits.map((hit: any) => hit._source as Asset)
+            const assetNames = assets.map((asset) => asset.metadata.name)
+            vscode.window.showInformationMessage(`Found assets: ${assetNames.join(', ')}`)
+          } else {
+            vscode.window.showInformationMessage('No assets found.')
+          }
+        } catch (error) {
+          vscode.window.showErrorMessage(`Error searching assets: ${error}`)
+        }
+      }
+    }
+  )
 
-	context.subscriptions.push(disposable);
+  // Command to get asset details
+  let getAssetDetails = vscode.commands.registerCommand(
+    'ocean-protocol.getAssetDetails',
+    async () => {
+      const did = await vscode.window.showInputBox({
+        prompt: 'Enter DID of the Ocean asset'
+      })
+      if (did) {
+        try {
+          const config = getOceanConfig()
+          const aquarius = new Aquarius(config.metadataCacheUri)
+          const asset = await aquarius.resolve(did)
+          if (asset) {
+            const details = `
+                        Name: ${asset.metadata.name}
+                        Type: ${asset.metadata.type}
+                        Description: ${asset.metadata.description}
+                        Author: ${asset.metadata.author}
+                    `
+            vscode.window.showInformationMessage(details)
+          } else {
+            vscode.window.showInformationMessage('Asset not found.')
+          }
+        } catch (error) {
+          vscode.window.showErrorMessage(`Error getting asset details: ${error}`)
+        }
+      }
+    }
+  )
+
+  context.subscriptions.push(searchAssets, getAssetDetails)
 }
 
-// This method is called when your extension is deactivated
+function getOceanConfig(): Config {
+  const configHelper = new ConfigHelper()
+  return configHelper.getConfig(1) // Assuming Ethereum mainnet, change as needed
+}
+
 export function deactivate() {}
