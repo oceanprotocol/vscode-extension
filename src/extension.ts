aeeb12c43381e7f137591ba8b8035a0d53c8e28b
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-10-03 15:40:53 +0300

    Setting up minimal ocean node

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,64 @@ import { ethers } from 'ethers'
 import * as fs from 'fs'
 import { createAsset } from './helpers/publish'
 import fetch from 'cross-fetch'
+import { spawn } from 'child_process'
+import * as path from 'path'
 
 globalThis.fetch = fetch
 
+let oceanNode: any
+
+async function startOceanNode(context: vscode.ExtensionContext): Promise<void> {
+  const oceanNodePath = path.join(
+    context.extensionPath,
+    'node_modules',
+    '.bin',
+    'ocean-node'
+  )
+
+  const env = {
+    ...process.env,
+    PRIVATE_KEY: '0x0000000000000000000000000000000000000000000000000000000000000001', // Replace with a valid private key
+    HTTP_API_PORT: '8000',
+    INTERFACES: '["HTTP"]',
+    RPCS: JSON.stringify({
+      '11155111': {
+        rpc: 'https://eth-sepolia.public.blastapi.io',
+        chainId: 11155111,
+        network: 'sepolia',
+        chunkSize: 100
+      }
+    }),
+    DB_URL: 'http://localhost:8108/?apiKey=xyz',
+    IPFS_GATEWAY: 'https://ipfs.io/',
+    ARWEAVE_GATEWAY: 'https://arweave.net/',
+    LOG_LEVEL: 'debug'
+  }
+
+  oceanNode = spawn(oceanNodePath, [], { env })
+
+  oceanNode.stdout.on('data', (data) => {
+    console.log(`Ocean Node stdout: ${data}`)
+  })
+
+  oceanNode.stderr.on('data', (data) => {
+    console.error(`Ocean Node stderr: ${data}`)
+  })
+
+  oceanNode.on('close', (code) => {
+    console.log(`Ocean Node process exited with code ${code}`)
+  })
+
+  vscode.window.showInformationMessage('Ocean Node started')
+}
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('Ocean Protocol extension is now active!')
+
+  // Start Ocean Node
+  startOceanNode(context)
+    .then(() => console.log('Ocean Node started successfully'))
+    .catch((error) => console.error('Failed to start Ocean Node:', error))
 
   const provider = new OceanProtocolViewProvider(context.extensionUri)
 
@@ -159,4 +212,10 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(getAssetDetails, publishAsset)
 }
 
-export function deactivate() {}
+export function deactivate() {
+  if (oceanNode) {
+    oceanNode.kill()
+    console.log('Ocean Node process terminated')
+  }
+  console.log('Ocean Protocol extension is now deactivated!')
+}
