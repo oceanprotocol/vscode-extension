dc9ac21b362fecc832ec03ea6d4062c2ad886de5
mariacarmina <mariacarmina.cretu@gmail.com>
2024-10-07 12:51:16 +0300

    fix import.

--- src/helpers/download.ts
+++ src/helpers/download.ts
@@ -1,49 +1,47 @@
 import {
-    Aquarius,
-    Datatoken,
-    ProviderInstance,
-    ConfigHelper,
-    orderAsset
+  Aquarius,
+  Datatoken,
+  ProviderInstance,
+  ConfigHelper,
+  orderAsset
 } from '@oceanprotocol/lib'
 import { Signer } from 'ethers'
-import fs from 'fs'
-import * as path from "path"
+import { promises as fs } from 'fs'
+import * as path from 'path'
 
-
-  export async function downloadFile(
-	url: string,
-	downloadPath: string,
-	index?: number
+export async function downloadFile(
+  url: string,
+  downloadPath: string,
+  index?: number
 ): Promise<any> {
-	const response = await fetch(url);
-	if (!response.ok) {
-		throw new Error("Response error.");
-	}
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error('Response error.')
+  }
 
-	const defaultName = !isNaN(index) && index > -1 ? `file_${index}.out` : 'file.out'
-	let filename: string
+  const defaultName = !isNaN(index) && index > -1 ? `file_${index}.out` : 'file.out'
+  let filename: string
 
-	try {
-		// try to get it from headers
-		filename = response.headers
-			.get("content-disposition")
-			.match(/attachment;filename=(.+)/)[1];
-	} catch {
-		filename = defaultName;
-	}
+  try {
+    // try to get it from headers
+    filename = response.headers
+      .get('content-disposition')
+      .match(/attachment;filename=(.+)/)[1]
+  } catch {
+    filename = defaultName
+  }
 
-	const filePath = path.join(downloadPath, filename);
-	const data = await response.arrayBuffer();
+  const filePath = path.join(downloadPath, filename)
+  const data = await response.arrayBuffer()
 
-	try {
-		await fs.writeFile(filePath, Buffer.from(data));
-	} catch (err) {
-		throw new Error("Error while saving the file:", err.message);
-	}
+  try {
+    await fs.writeFile(filePath, Buffer.from(data))
+  } catch (err) {
+    throw new Error('Error while saving the file:', err.message)
+  }
 
-	return filename
+  return filename
 }
-
 
 export async function download(
   did: string,
@@ -51,60 +49,51 @@ export async function download(
   pathString: string = '.',
   aquariusInstance: Aquarius,
   macOsProviderUrl?: string,
-  providerUrl?: string,
+  providerUrl?: string
 ) {
-    const dataDdo = await aquariusInstance.waitForAqua(did);
-    if (!dataDdo) {
-        console.error(
-            "Error fetching DDO " + did + ".  Does this asset exists?"
-        )
-        return
-    }
-    let providerURI
-    if (!providerUrl) {
-        providerURI = macOsProviderUrl && dataDdo.chainId === 8996
+  const dataDdo = await aquariusInstance.waitForAqua(did)
+  if (!dataDdo) {
+    console.error('Error fetching DDO ' + did + '.  Does this asset exists?')
+    return
+  }
+  let providerURI
+  if (!providerUrl) {
+    providerURI =
+      macOsProviderUrl && dataDdo.chainId === 8996
         ? macOsProviderUrl
         : dataDdo.services[0].serviceEndpoint
-    } else {
-        providerURI = providerUrl
-    }
-    console.log("Downloading asset using provider: ", providerURI)
-    const { chainId } = await owner.provider.getNetwork()
-    console.log('Chain ID:', chainId)
-    const config = new ConfigHelper().getConfig(chainId)
-    console.log('Config:', config)
-    const datatoken = new Datatoken(owner, chainId)
+  } else {
+    providerURI = providerUrl
+  }
+  console.log('Downloading asset using provider: ', providerURI)
+  const { chainId } = await owner.provider.getNetwork()
+  console.log('Chain ID:', chainId)
+  const config = new ConfigHelper().getConfig(chainId)
+  console.log('Config:', config)
+  const datatoken = new Datatoken(owner, chainId)
 
-    const tx = await orderAsset(
-        dataDdo,
-        owner,
-        config,
-        datatoken,
-        providerURI
-    )
+  const tx = await orderAsset(dataDdo, owner, config, datatoken, providerURI)
 
-    if (!tx) {
-        console.error(
-            "Error ordering access for " + did + ".  Do you have enough tokens?"
-        )
-        return
-    }
+  if (!tx) {
+    console.error('Error ordering access for ' + did + '.  Do you have enough tokens?')
+    return
+  }
 
-    const orderTx = await tx.wait();
+  const orderTx = await tx.wait()
 
-    const urlDownloadUrl = await ProviderInstance.getDownloadUrl(
-        dataDdo.id,
-        dataDdo.services[0].id,
-        0,
-        orderTx.transactionHash,
-        providerURI,
-        owner
-    );
-    try {
-        const path = pathString ? pathString : '.';
-        const { filename } = await downloadFile(urlDownloadUrl, path);
-        console.log("File downloaded successfully:", path + "/" + filename);
-    } catch (e) {
-        console.log(`Download url dataset failed: ${e}`);
-    }
+  const urlDownloadUrl = await ProviderInstance.getDownloadUrl(
+    dataDdo.id,
+    dataDdo.services[0].id,
+    0,
+    orderTx.transactionHash,
+    providerURI,
+    owner
+  )
+  try {
+    const path = pathString ? pathString : '.'
+    const { filename } = await downloadFile(urlDownloadUrl, path)
+    console.log('File downloaded successfully:', path + '/' + filename)
+  } catch (e) {
+    console.log(`Download url dataset failed: ${e}`)
+  }
 }
