a55e7e17b2826cff456ee15798bb9d68ae861f6a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-02-05 16:57:13 +0300

    Adding test for metadata

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -18,7 +18,7 @@ suite('Ocean Protocol Extension Test Suite', () => {
   })
 
   test('Extension should be present', () => {
-    assert.ok(vscode.extensions.getExtension('ocean-protocol'))
+    assert.ok(vscode.extensions.getExtension('ocean-protocol.ocean-protocol'))
   })
 
   test('computeStart should handle JavaScript algorithm correctly', async () => {
