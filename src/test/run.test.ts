654eb93c45f1995b488ca17ee3f10a95151f60cf
giurgiur99 <giurgiur99@gmail.com>
2026-03-09 15:24:44 +0200

    mock deps

--- src/test/run.test.ts
+++ src/test/run.test.ts
@@ -7,4 +7,21 @@ const p2pMock = {
 const p2pPath = require.resolve('../helpers/p2p')
 mockRequire(p2pPath, p2pMock)
 
+// Mock ESM-only packages that cannot be required in CJS test context
+mockRequire('@multiformats/multiaddr', {
+  multiaddr: (addr: string) => addr,
+  isMultiaddr: () => true
+})
+
+mockRequire('uint8arrays/from-string', {
+  fromString: (s: string) => Buffer.from(s)
+})
+
+mockRequire('@libp2p/utils', {
+  lpStream: () => ({
+    write: async () => {},
+    read: async () => new Uint8Array()
+  })
+})
+
 require('./extension.test')
